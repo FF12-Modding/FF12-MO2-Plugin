0cfd36c73b693a41e565f4bb4afb26f1c27a9543
ffgriever-pl <53974091+ffgriever-pl@users.noreply.github.com>
2024-07-24 12:29:00 +0200

    Add first plugin version.

---
+++ game_ff12.py
@@ -0,0 +1,95 @@
+import mobase
+
+from ..basic_features import BasicModDataChecker, GlobPatterns
+from ..basic_game import BasicGame
+from ..basic_features.utils import is_directory
+
+class FF12ModDataChecker(BasicModDataChecker):
+    def __init__(self, patterns: GlobPatterns = GlobPatterns()):
+        super().__init__(
+            GlobPatterns(
+                unfold=['*'],
+                delete=["*"],
+                valid=["x64", "mods", "dxgi.dll", "dinput8.dll", "launcher.dll"],
+                move={"scripts":        "x64/",
+                      "modules":        "x64/",
+                      "gamedata":       "mods/deploy/ff12data/",
+                      "jsondata":       "mods/deploy/ff12data/",
+                      "prefetchdata":   "mods/deploy/ff12data/",
+                      "ps2data":        "mods/deploy/ff12data/",
+                      },
+            ).merge(patterns)
+        )
+
+    def dataLooksValid(
+        self, filetree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
+        status = mobase.ModDataChecker.VALID
+
+        rp = self._regex_patterns
+        for entry in filetree:
+            name = entry.name().casefold()
+
+            if rp.valid.match(name):
+                if status is mobase.ModDataChecker.INVALID:
+                    status = mobase.ModDataChecker.VALID
+
+            elif rp.move_match(name) is not None:
+                status = mobase.ModDataChecker.FIXABLE
+
+            elif rp.unfold.match(name) and is_directory(entry):
+                status = mobase.ModDataChecker.FIXABLE
+                new_status = self.dataLooksValid(entry)
+                if new_status is not mobase.ModDataChecker.VALID:
+                    status = new_status
+
+            elif rp.delete.match(name) is not None:
+                status = mobase.ModDataChecker.FIXABLE
+
+            else:
+                status = mobase.ModDataChecker.INVALID
+                break
+        return status
+
+    def fix(self, filetree: mobase.IFileTree) -> mobase.IFileTree:
+        rp = self._regex_patterns
+        for entry in list(filetree):
+            name = entry.name().casefold()
+
+            if rp.valid.match(name):
+                continue
+
+            elif (move_key := rp.move_match(name)) is not None:
+                target = self._file_patterns.move[move_key]
+                filetree.move(entry, target)
+
+            elif rp.unfold.match(name) and is_directory(entry):
+                filetree.merge(entry)
+                entry.detach()
+                self.fix(filetree)
+
+            elif rp.delete.match(name):
+                entry.detach()
+
+        return filetree
+
+class FF12TZAGame(BasicGame):
+    Name = "Final Fantasy XII TZA Support Plugin"
+    Author = "ffgriever"
+    GameName = "Final Fantasy XII The Zodiac Age"
+    GameShortName = "finalfantasy12"
+    GameNexusName = "finalfantasy12"
+    GameBinary = "x64/FFXII_TZA.exe"
+    GameDataPath = "%GAME_PATH%"
+    GameSteamId = 595520
+
+    def __init__(self):
+        super().__init__()
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._featureMap[mobase.ModDataChecker] = FF12ModDataChecker()
+        return True
+
+    def version(self):
+        return mobase.VersionInfo(0, 1, 0, mobase.ReleaseType.BETA)
