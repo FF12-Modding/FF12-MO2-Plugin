4d2fa8851708c6178995354ae286a4c6cafc48b2
Xeavin <64920094+Xeavin@users.noreply.github.com>
2025-08-07 23:21:55 +0200

    Return early on input validation in date helpers.

--- ff12/DateHelper.py
+++ ff12/DateHelper.py
@@ -1,18 +1,21 @@
 from datetime import datetime
 
 def get_date_time_from_iso(date_iso: str) -> str:
-    if date_iso:
-        try:
-            dt = datetime.fromisoformat(date_iso.replace("Z", "+00:00"))
-            return dt.strftime("%Y-%m-%d %H:%M UTC")
-        except Exception:
-            return date_iso
+    if not isinstance(date_iso, str):
+        return str(date_iso)
 
+    try:
+        dt = datetime.fromisoformat(date_iso.replace("Z", "+00:00"))
+        return dt.strftime("%Y-%m-%d %H:%M UTC")
+    except Exception:
+        return date_iso
 
 def get_date_from_iso(date_iso: str) -> str:
-    if date_iso:
-        try:
-            dt = datetime.fromisoformat(date_iso.replace("Z", "+00:00"))
-            return dt.strftime("%Y-%m-%d")
-        except Exception:
-            return date_iso
+    if not isinstance(date_iso, str):
+        return str(date_iso)
+
+    try:
+        dt = datetime.fromisoformat(date_iso.replace("Z", "+00:00"))
+        return dt.strftime("%Y-%m-%d")
+    except Exception:
+        return date_iso
